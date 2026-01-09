eb089bf331917937df246bdba276d0613c3400e8
xy.kong <xy.kong@gmail.com>
2026-01-09 12:49:26 +0800

    Update cask to version 1.1.61

--- Casks/markdown-preview-enhanced.rb
+++ Casks/markdown-preview-enhanced.rb
@@ -1,18 +1,18 @@
-cask "markdown-preview-enhanced" do
-  version "1.0.0"
-  sha256 :no_check
+cask 'markdown-preview-enhanced' do
+  version '1.1.61'
+  sha256 '2d2b3e0ffbd6cde8125cd7457f1265e6fdf6a87ab659e345e6eeb22d49f07221'
 
   url "https://github.com/xykong/markdown-quicklook/releases/download/v#{version}/MarkdownPreviewEnhanced.dmg"
-  name "Markdown Preview Enhanced"
-  desc "Markdown Preview Enhanced for macOS QuickLook"
-  homepage "https://github.com/xykong/markdown-quicklook"
+  name 'Markdown Preview Enhanced'
+  desc 'Markdown Preview Enhanced for macOS QuickLook'
+  homepage 'https://github.com/xykong/markdown-quicklook'
 
-  app "Markdown Preview Enhanced.app"
+  app 'Markdown Preview Enhanced.app'
 
   zap trash: [
-    "~/Library/Application Scripts/com.xykong.Markdown",
-    "~/Library/Containers/com.xykong.Markdown",
-    "~/Library/Application Scripts/com.xykong.Markdown.QuickLook",
-    "~/Library/Containers/com.xykong.Markdown.QuickLook",
+    '~/Library/Application Scripts/com.xykong.Markdown',
+    '~/Library/Containers/com.xykong.Markdown',
+    '~/Library/Application Scripts/com.xykong.Markdown.QuickLook',
+    '~/Library/Containers/com.xykong.Markdown.QuickLook'
   ]
 end
