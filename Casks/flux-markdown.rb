6f25a5a14fa783ba0d5b7af65804a7ad0d08bd0d
xy.kong <xy.kong@gmail.com>
2026-02-18 16:57:07 +0800

    feat(cask): add FluxMarkdown v1.14.164 cask

---
+++ Casks/flux-markdown.rb
@@ -0,0 +1,51 @@
+cask 'flux-markdown' do
+  version '1.14.164'
+  sha256 '7a3e55222c3cf7f4212703e296959324c4ac1548ac28c5bc110c513ab7738e1b'
+
+  url "https://github.com/xykong/flux-markdown/releases/download/v#{version}/FluxMarkdown.dmg"
+  name 'FluxMarkdown'
+  desc 'Beautiful Markdown previews in macOS Finder QuickLook'
+  homepage 'https://github.com/xykong/flux-markdown'
+
+  auto_updates true
+
+  livecheck do
+    url 'https://raw.githubusercontent.com/xykong/flux-markdown/master/appcast.xml'
+    strategy :sparkle, &:short_version
+  end
+
+  app 'FluxMarkdown.app'
+
+  postflight do
+    system_command '/usr/bin/xattr',
+                   args: ['-cr', "#{appdir}/FluxMarkdown.app"],
+                   sudo: false
+
+    system_command '/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister',
+                   args: ['-f', "#{appdir}/FluxMarkdown.app"],
+                   sudo: false
+
+    system_command '/usr/bin/qlmanage',
+                   args: ['-r'],
+                   sudo: false
+
+    # Open the app in background to trigger QuickLook extension registration
+    system_command '/usr/bin/open',
+                   args: ['-g', "#{appdir}/FluxMarkdown.app", '--args', '--register-only'],
+                   sudo: false
+  end
+
+  caveats <<~EOS
+    If the QuickLook extension does not work immediately:
+      1. Run 'qlmanage -r' in Terminal.
+      2. Restart Finder (Force Quit > Finder > Relaunch).
+      3. Set 'FluxMarkdown.app' as the default app for .md files.
+  EOS
+
+  zap trash: [
+    '~/Library/Application Scripts/com.xykong.Markdown',
+    '~/Library/Containers/com.xykong.Markdown',
+    '~/Library/Application Scripts/com.xykong.Markdown.QuickLook',
+    '~/Library/Containers/com.xykong.Markdown.QuickLook'
+  ]
+end
